fa351982c3bbf0af7fa073ae9667c13555c73410
Alex-Kostov <alex89773@gmail.com>
2023-01-26 21:08:42 +0200

    fix indentation

--- src/model/transactions.ts
+++ src/model/transactions.ts
@@ -1,46 +1,46 @@
 import mongoose, { Schema, Types } from 'mongoose';
 
 export interface TransactionImpl {
-  amount: number;
-  description?: string;
-  transactionType: string;
-  category: string;
-  vault: Types.ObjectId;
-  date: Date;
+	amount: number;
+	description?: string;
+	transactionType: string;
+	category: string;
+	vault: Types.ObjectId;
+	date: Date;
 }
 
 const transactionSchema = new Schema<TransactionImpl>({
-  amount: {
-    type: Number,
-    required: true,
-    immutable: true,
-    min: [0, 'Amount must be positive number'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  transactionType: {
-    type: String,
-    required: true,
-    enum: ['income', 'expense'],
-  },
-  category: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  vault: {
-    type: Schema.Types.ObjectId,
-    ref: 'Vault',
-  },
-  date: {
-    type: Date,
-    required: true
-  },
+	amount: {
+		type: Number,
+		required: true,
+		immutable: true,
+		min: [0, 'Amount must be positive number'],
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	transactionType: {
+		type: String,
+		required: true,
+		enum: ['income', 'expense'],
+	},
+	category: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	vault: {
+		type: Schema.Types.ObjectId,
+		ref: 'Vault',
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
 });
 
 export default mongoose.model<TransactionImpl>(
-  'Transaction',
-  transactionSchema
+	'Transaction',
+	transactionSchema
 );
--- src/model/vaults.ts
+++ src/model/vaults.ts
@@ -1,25 +1,25 @@
 import mongoose, { Schema, Types } from 'mongoose';
 
 export interface VaultImpl {
-  name: string;
-  type: string;
-  currency: Types.ObjectId;
-  _id?: Types.ObjectId;
-  __v?: number;
+	name: string;
+	type: string;
+	currency: Types.ObjectId;
+	_id?: Types.ObjectId;
+	__v?: number;
 }
 
 const vaultSchema = new Schema<VaultImpl>({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  type: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  currency: { type: Schema.Types.ObjectId, ref: 'Currency' }
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	type: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	currency: { type: Schema.Types.ObjectId, ref: 'Currency' },
 });
 
 export default mongoose.model<VaultImpl>('Vault', vaultSchema);
