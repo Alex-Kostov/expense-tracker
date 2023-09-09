b26360a1edc7b2ffc535f8e479960c30823993d5
Alex Kostov <alex89773@gmail.com>
2023-09-09 22:30:51 +0300

    feat: adding material Dashboard

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Dashboard from "./pages/Dashboard";
 
 function App() {
     return (
         <Router>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Dashboard />} />
                 {/*<Route path="/expenses" element={<Expenses />} />*/}
                 {/*<Route path="/incomes" element={<Incomes />} />*/}
                 {/* Add more routes as needed */}
